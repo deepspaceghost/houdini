6a9a5a5d03c313ba7c1614a517dd28304f059f87
deepspaceghost <ghostblac@protonmail.com>
2021-02-28 12:06:14 -0500

    converted num_check to an internal service, implemented argparse, cleaned up code, brought Houdini up to a cli program

--- main/houdini.py
+++ main/houdini.py
@@ -1,4 +1,6 @@
-import sys
+import argparse
+import json
+# import sys
 
 """
 Project Title: Find the character of an ASCII code
@@ -9,20 +11,23 @@ Project Description: Write a program that receives an ASCII
 code (an integer between 0 and 127) and displays
 its character. For example, if the user enters 97, the
 program displays the character a.
-"""
 
-# Input: Enter an ASCII code: 69 [enter]
+Input: Enter an ASCII code: 69 [enter]
 
-# Input constraints: Is the input an integer? Between 0 and 127?
+Input constraints: Is the input an integer? Between 0 and 127?
 
-# Compute
-
-"""
+Compute
 """
 
 # Current task(s): as of 1:39am, 1/25/2021, create tests for individual functions
 
 # Output: The character is E
+
+"""
+git add *
+git commit -m "some message"
+git push origin dev
+"""
 
 
 def num_check(chango):
@@ -47,23 +52,21 @@ if __name__ == "__main__":
     """
     """
 
-    print("And now, for my next trick, give me a number, between 0 and 127.")
-    print("In exchange, I will make a letter appear in its place.")
+    data = {"president": {"name": "Zaphod Beeblebrox", "species": "Betelgeusian"}}
+    with open("data_file.json", "w") as write_file:
+        json.dump(data, write_file)
 
-    print(sys.argv)
-    presto = sys.argv[2]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--voila",
+                        action="store",
+                        nargs=1,
+                        help="Usage: ./houdini.py --voila [chango]",
+                        dest="voila_args")
 
-    try:
-        chango = int(presto)
+    args = parser.parse_args()
 
-    except ValueError as err:
-        print("INFO: Could not convert data into a integer.")
-        print("DEBUG: {0}".format(err))
-
-        exit()
-
+    chango = int(args.voila_args[0])
     if num_check(chango):
-        print("This number is within range!")
         voila(chango)
     else:
-        print("This number is not within range!")
+        print("For this trick, I require a number between 0 and 127.")
